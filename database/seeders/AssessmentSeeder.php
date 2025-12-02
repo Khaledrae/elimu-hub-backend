a5dcc5a2564981f9d7d5dddadeb08ae91111ac02
Julius.Matta <Julius.Matta@KELTHSEIT052290>
2025-12-02 06:52:47 +0300

    Assessment 2

--- database/seeders/AssessmentSeeder.php
+++ database/seeders/AssessmentSeeder.php
@@ -4,20 +4,25 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use App\Models\Assessment;
+use App\Models\Teacher;
 
 class AssessmentSeeder extends Seeder
 {
     public function run(): void
     {
+        $subject = 'English';
+        $teacher = Teacher::where('subject_specialization', $subject)->first();
+        $user_id = $teacher ? $teacher->id : null;
+
         $assessment = Assessment::create([
-            'lesson_id'       => 1,             // Adjust if needed
-            'teacher_id'      => 26,
+            'lesson_id'       => 11,             // Adjust if needed
+            'teacher_id'      => $user_id,
             'title'           => 'English Basics Quiz – Alphabet & Sounds',
             'instructions'    => 'Answer all questions. Choose the best option.',
             'type'            => 'quiz',
             'total_marks'     => 10,
-            'duration_minutes'=> 10,
-            'status'          => 'active',
+            'duration_minutes' => 10,
+            'status'          => 'published',
         ]);
 
         // Store the id for question seeding
